891ef944f13503a9f8fe48bcabf3a0b5735f9001
Maksym Lukashenko <maksym.lukashenko@gmail.com>
2018-01-30 23:57:40 +0300

    Android

---
+++ AndroidTests/Pages/ViewsPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Compatibility;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+
+namespace AndroidTests.Pages
+{
+    public class ViewsPage: MainPage
+    {
+        protected void GoToRotatingButton()
+        {
+            GoToViews();
+            driver.Swipe(565, 594, 565, 473, 0);
+            driver.Swipe(554, 858, 554, 745, 0);
+            driver.FindElementByAccessibilityId("Rotating Button").Click();
+        }
+    }
+}
---
+++ AndroidTests/SeekBarTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Compatibility;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+using OpenQA.Selenium.Appium.MultiTouch;
+using AndroidTests.Pages;
+
+namespace AndroidTests
+{
+    [TestFixture]
+    public class SeekBarTests:ViewsPage
+    {
+        private static readonly object[] TestData =
+        {
+            new object[] { "io.appium.android.apis:id/scaleX",0.8}
+        };
+
+
+        [Test, TestCaseSource(nameof(TestData))]
+        //[Test, TestCaseSource(nameof(TestData))]
+        public void SearchAndMoveSeekBar(string seekBarId, double percentToMove)
+        {
+            GoToRotatingButton();
+            if (percentToMove > 1)
+            {
+                throw new Exception("percentToMove variable can be set only in [0,1] range");
+            }
+            IWebElement seekBar = driver.FindElementById(seekBarId);
+            driver.FindElementById(seekBarId).Tap(1, 100);
+            //Get start point of seekbar.
+            double startX = seekBar.Location.X;
+            double yPos = seekBar.Location.Y;
+            double bound = seekBar.Size.Width;
+            double moveByX = startX + bound * percentToMove;
+            //Moving seekbar using TouchAction class.
+            TouchAction act = new TouchAction(driver);
+            act.LongPress(startX, yPos).MoveTo(startX - bound / 2, yPos).Release().Perform();
+            startX = seekBar.Location.X;
+            act.LongPress(startX, yPos).MoveTo(moveByX, yPos).Release().Perform();
+        }
+    }
+}
